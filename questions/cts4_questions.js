114f7f9b70039138e6de11bce90c7e98a073cc81
Alec <56179545+alecdvor@users.noreply.github.com>
2025-09-16 10:49:21 -0400

    Update cts4_questions.js

--- questions/cts4_questions.js
+++ questions/cts4_questions.js
@@ -2,256 +2,179 @@ var cts4_questions = {
     "questions": [
         {
             "id": "cts4_1",
+            "question": "After presenting a proposal, what is the correct plan of action for following up with the client?",
+            "options": [
+                "Regularly, until a decision is made"
+            ],
+            "answer": "Regularly, until a decision is made"
+        },
+        {
+            "id": "cts4_2",
+            "question": "When training a new user how to operate the solution that has been provided, which of the following is a best practice?",
+            "options": [
+                "Provide complete user documentation prior to training"
+            ],
+            "answer": "Provide complete user documentation prior to training"
+        },
+        {
+            "id": "cts4_3",
+            "question": "What is a benefit of offering a free training session for a client with new equipment?",
+            "options": [
+                "It is a positive approach for a long-term partnership"
+            ],
+            "answer": "It is a positive approach for a long-term partnership"
+        },
+        {
+            "id": "cts4_4",
             "question": "A dimmer rack with a constant load capability is powering twenty four incandescent lights during a stage production. The dimmer rack is running on a steady 230VAC supply and averaging approximately 98 amps draw. Suddenly the line voltage drops to 211VAC. Calculate the expected amperage draw.",
             "options": [
-                "98 amps",
-                "107 amps",
-                "90 amps",
-                "85 amps"
+                "90 amps"
             ],
             "answer": "90 amps"
         },
         {
-            "id": "cts4_2",
+            "id": "cts4_5",
             "question": "A successful project is an AV system that meets the original design, fulfills the needs of the, and remains in proper working order.",
             "options": [
-                "Installer, manufacturer",
-                "Salesperson, owner",
-                "Intention, user",
-                "Budget, timeline"
+                "Intention, user"
             ],
             "answer": "Intention, user"
         },
         {
-            "id": "cts4_3",
+            "id": "cts4_6",
             "question": "Which type of warranty typically involves additional costs and adjustments to meet the owner's needs?",
             "options": [
-                "Manufacturer's warranty",
-                "Implied warranty",
-                "Service agreement",
-                "Standard 90-day warranty"
+                "Service agreement"
             ],
             "answer": "Service agreement"
         },
         {
-            "id": "cts4_4",
+            "id": "cts4_7",
             "question": "When training users during the System Commissioning and Training Phase, groups allow for more hands-on time with the system.",
             "options": [
-                "Larger (20 or more)",
-                "Smaller (up to six)",
-                "Mixed-skill-level",
-                "Management-only"
+                "Smaller (up to six)"
             ],
             "answer": "Smaller (up to six)"
         },
         {
-            "id": "cts4_5",
+            "id": "cts4_8",
             "question": "When working with a new technician, what is the best way to maintain quality control?",
             "options": [
-                "Assume they know all company standards",
-                "Have them work alone on their first project",
-                "Provide training, enforce standards, and conduct observations",
-                "Only assign them to non-critical tasks"
+                "Provide training, enforce standards, and conduct observations"
             ],
             "answer": "Provide training, enforce standards, and conduct observations"
         },
         {
-            "id": "cts4_6",
+            "id": "cts4_9",
             "question": "How do you maintain a proper focus on customer needs and service?",
             "options": [
-                "Tell the customer their concerns are not valid",
-                "Wait until the end of the project to address any issues",
-                "Listen to customer concerns and try to accommodate immediately if it is feasible.",
-                "Charge the customer for any time spent addressing their concerns"
+                "Listen to customer concerns and try to accommodate immediately if it is feasible."
             ],
             "answer": "Listen to customer concerns and try to accommodate immediately if it is feasible."
         },
         {
-            "id": "cts4_7",
+            "id": "cts4_10",
             "question": "During the commissioning process for an AV system installation, how will the AV technician confirm thatthe video display system is working properly?",
             "options": [
-                "Ask the client if the image looks okay",
-                "Turn the display on and off to see if it powers up",
-                "Connect display devices to a test signal generator, calibrate at points along the path.",
-                "Measure the voltage at the power outlet"
+                "Connect display devices to a test signal generator, calibrate at points along the path."
             ],
             "answer": "Connect display devices to a test signal generator, calibrate at points along the path."
         },
         {
-            "id": "cts4_8",
+            "id": "cts4_11",
             "question": "What are the two main components of a projector that need to be maintained",
             "options": [
-                "Lens and remote control",
-                "Air Filter and Lamps",
-                "Power cord and mounting bracket",
-                "Zoom and focus rings"
+                "Air Filter and Lamps"
             ],
             "answer": "Air Filter and Lamps"
         },
         {
-            "id": "cts4_9",
+            "id": "cts4_12",
             "question": "Why is it very important to document all changes or update activities on an AV system whenever they are performed?",
             "options": [
-                "To increase the amount of billable hours",
-                "To have a record for employee performance reviews",
-                "To manage and maintain the system successfully over time.",
-                "It is not important to document these changes"
+                "To manage and maintain the system successfully over time."
             ],
             "answer": "To manage and maintain the system successfully over time."
         },
         {
-            "id": "cts4_10",
+            "id": "cts4_13",
             "question": "How does the AV manager maintain good relationships with suppliers and vendors?",
             "options": [
-                "Always demand the lowest possible price",
-                "Pay invoices as late as possible",
-                "Document all transactions, resolve concerns in a professional manner.",
-                "Avoid communicating with vendors directly"
+                "Document all transactions, resolve concerns in a professional manner."
             ],
             "answer": "Document all transactions, resolve concerns in a professional manner."
         },
         {
-            "id": "cts4_11",
+            "id": "cts4_14",
+            "question": "What should be done when you receive a call that there is no sound in a room for an AV event?",
+            "options": [
+                "Check microphone, batteries, and power switches to assure they are on."
+            ],
+            "answer": "Check microphone, batteries, and power switches to assure they are on."
+        },
+        {
+            "id": "cts4_15",
             "question": "Employee evaluations must include:",
             "options": [
-                "Only positive feedback",
-                "Comparison to other employees' performance",
-                "Documentation of All standards",
-                "The employee's personal opinions"
+                "Documentation of All standards"
             ],
             "answer": "Documentation of All standards"
         },
         {
-            "id": "cts4_12",
+            "id": "cts4_16",
             "question": "When properly addressing problems with staff performance or behavior the supervisor must be sure to:",
             "options": [
-                "Discuss the issues in a public setting",
-                "Use vague and general language",
-                "Clearly identify and document the problem behavior and instances of it.",
-                "Wait until the annual review to bring up the problem"
+                "Clearly identify and document the problem behavior and instances of it."
             ],
             "answer": "Clearly identify and document the problem behavior and instances of it."
         },
         {
-            "id": "cts4_13",
+            "id": "cts4_17",
             "question": "A laptop input on a lectern is not working. The interface is connected to a 5 BNC wall plate. The interface is receiving signal and the projector is on the correct input. What is the next step in troubleshooting theissue?",
             "options": [
-                "Replace the projector",
-                "Reverse the H and V sync cables.",
-                "Reboot the laptop",
-                "Replace the entire cable run"
+                "Reverse the H and V sync cables."
             ],
             "answer": "Reverse the H and V sync cables."
         },
         {
-            "id": "cts4_14",
+            "id": "cts4_18",
+            "question": "What should be done if a touch screen control panel is reportedly locked up, frozen, or other wise reported to be not functioning?",
+            "options": [
+                "Check all connectors, confirm the screen is not in power saver mode."
+            ],
+            "answer": "Check all connectors, confirm the screen is not in power saver mode."
+        },
+        {
+            "id": "cts4_19",
             "question": "What should you do if the audio system is emitting a high frequency hiss, or a waterfall sound?",
             "options": [
-                "Turn the main volume down",
-                "Check inputs on the mixer, make sure not set too low, check all input gains",
-                "Replace all the speakers",
-                "Assume it is normal and ignore it"
+                "Check inputs on the mixer, make sure not set too low, check all input gains"
             ],
             "answer": "Check inputs on the mixer, make sure not set too low, check all input gains"
         },
         {
-            "id": "cts4_15",
-            "question": "When including GUI Layouts into the Design Documents, which of the following should also be included for optimum usability?",
-            "options": [
-                "The programmer's contact information",
-                "The source code for the GUI",
-                "Button-by-Button Functionality",
-                "A list of alternative color schemes"
-            ],
-            "answer": "Button-by-Button Functionality"
-        },
-        {
-            "id": "cts4_16",
-            "question": "How often must a Certified Technology Specialist (CTS) holder renew their certification through continuing education?",
-            "options": [
-                "Every year",
-                "Every three years",
-                "Every five years",
-                "Only once upon initial certification"
-            ],
-            "answer": "Every three years"
-        },
-        {
-            "id": "cts4_17",
-            "question": "How many Renewal Units (RUs) must a CTS holder complete within their certification period to be eligible for renewal?",
-            "options": [
-                "Fifteen (15)",
-                "Sixty (60)",
-                "Thirty (30)",
-                "Forty-five (45)"
-            ],
-            "answer": "Thirty (30)"
-        },
-        {
-            "id": "cts4_18",
-            "question": "When a client reports a technical issue with an AV system, what is the most critical information to obtain first to begin the formal troubleshooting process?",
-            "options": [
-                "The client's opinion on what is broken",
-                "A detailed description of the problem and any error messages",
-                "The original purchase date of the equipment",
-                "The name of the last person who used the system"
-            ],
-            "answer": "A detailed description of the problem and any error messages"
-        },
-        {
-            "id": "cts4_19",
-            "question": "A third party requests a copy of the final system programming files for a client's project. According to the principle of confidentiality, what is the correct action?",
-            "options": [
-                "Immediately provide the files to be helpful",
-                "Do not provide the files without written permission from the client",
-                "Provide the files but charge an administrative fee",
-                "Deny the request and refuse to explain why"
-            ],
-            "answer": "Do not provide the files without written permission from the client"
-        },
-        {
             "id": "cts4_20",
-            "question": "A client has lost their copy of the 'as-built' system drawings and requests a replacement. What is the standard industry practice?",
+            "question": "Good customer service is crucial for",
             "options": [
-                "Inform the client that drawings can never be re-issued",
-                "Redraw the entire system from scratch for a large fee",
-                "Provide a replacement copy, potentially for a small administrative fee",
-                "Tell the client to find the original copy they were given"
+                "Building good customer relationships"
             ],
-            "answer": "Provide a replacement copy, potentially for a small administrative fee"
+            "answer": "Building good customer relationships"
         },
         {
             "id": "cts4_21",
-            "question": "When a client has a formal complaint about a service provided, what is the best first step to ensure the issue is properly addressed?",
+            "question": "When is the best time to train users during the System Commissioning and Training Phase of a project?",
             "options": [
-                "Try to solve the problem over the phone immediately without documentation",
-                "Have the client submit the complaint in writing to create a formal record",
-                "Offer a discount on future services to smooth things over",
-                "Wait a few days to see if the client is still upset"
+                "After the system is complete"
             ],
-            "answer": "Have the client submit the complaint in writing to create a formal record"
+            "answer": "After the system is complete"
         },
         {
             "id": "cts4_22",
-            "question": "What is the CTS holder's primary responsibility regarding Renewal Units (RUs) for certification maintenance?",
+            "question": "When including GUI Layouts into the Design Documents, which of the following should also be included for optimum usability?",
             "options": [
-                "To attend every AVIXA-hosted event",
-                "To keep a personal record of all renewal units earned",
-                "To wait for AVIXA to send a complete record automatically",
-                "To only use one provider for all RUs"
+                "Button-by-Button Functionality"
             ],
-            "answer": "To keep a personal record of all renewal units earned"
-        },
-        {
-            "id": "cts4_23",
-            "question": "A supervisor assigns a CTS technician to a complex task that is beyond their experience. According to the CTS Code of Ethics and Conduct, what should the technician do?",
-            "options": [
-                "Attempt the task anyway to avoid looking inexperienced",
-                "Inform the supervisor that they should only undertake assignments for which they are capable",
-                "Delegate the task to a junior technician",
-                "Watch online videos to learn how to do it on the spot"
-            ],
-            "answer": "Inform the supervisor that they should only undertake assignments for which they are capable"
+            "answer": "Button-by-Button Functionality"
         }
     ]
-}
+};
