009e747e72efa3dc77b4f4d3e3daa75b828e0cd5
Alec <56179545+alecdvor@users.noreply.github.com>
2025-09-16 09:05:47 -0400

    Create cts4_questions.js

---
+++ questions/cts4_questions.js
@@ -0,0 +1,180 @@
+var cts4_questions = {
+    "questions": [
+        {
+            "id": "cts4_1",
+            "question": "After presenting a proposal, what is the correct plan of action for following up with the client?",
+            "options": [
+                "Regularly, until a decision is made"
+            ],
+            "answer": "Regularly, until a decision is made"
+        },
+        {
+            "id": "cts4_2",
+            "question": "When training a new user how to operate the solution that has been provided, which of the following is a best practice?",
+            "options": [
+                "Provide complete user documentation prior to training"
+            ],
+            "answer": "Provide complete user documentation prior to training"
+        },
+        {
+            "id": "cts4_3",
+            "question": "What is a benefit of offering a free training session for a client with new equipment?",
+            "options": [
+                "It is a positive approach for a long-term partnership"
+            ],
+            "answer": "It is a positive approach for a long-term partnership"
+        },
+        {
+            "id": "cts4_4",
+            "question": "A dimmer rack with a constant load capability is powering twenty four incandescent lights during a stage production. The dimmer rack is running on a steady 230VAC supply and averaging approximately 98 amps draw. Suddenly the line voltage drops to 211VAC. Calculate the expected amperage draw.",
+            "options": [
+                "90 amps"
+            ],
+            "answer": "90 amps"
+        },
+        {
+            "id": "cts4_5",
+            "question": "A successful project is an AV system that meets the original design, fulfills the needs of the, and remains in proper working order.",
+            "options": [
+                "Intention, user"
+            ],
+            "answer": "Intention, user"
+        },
+        {
+            "id": "cts4_6",
+            "question": "Which type of warranty typically involves additional costs and adjustments to meet the owner's needs?",
+            "options": [
+                "Service agreement"
+            ],
+            "answer": "Service agreement"
+        },
+        {
+            "id": "cts4_7",
+            "question": "When training users during the System Commissioning and Training Phase, groups allow for more hands-on time with the system.",
+            "options": [
+                "Smaller (up to six)"
+            ],
+            "answer": "Smaller (up to six)"
+        },
+        {
+            "id": "cts4_8",
+            "question": "When working with a new technician, what is the best way to maintain quality control?",
+            "options": [
+                "Provide training, enforce standards, and conduct observations"
+            ],
+            "answer": "Provide training, enforce standards, and conduct observations"
+        },
+        {
+            "id": "cts4_9",
+            "question": "How do you maintain a proper focus on customer needs and service?",
+            "options": [
+                "Listen to customer concerns and try to accommodate immediately if it is feasible."
+            ],
+            "answer": "Listen to customer concerns and try to accommodate immediately if it is feasible."
+        },
+        {
+            "id": "cts4_10",
+            "question": "During the commissioning process for an AV system installation, how will the AV technician confirm thatthe video display system is working properly?",
+            "options": [
+                "Connect display devices to a test signal generator, calibrate at points along the path."
+            ],
+            "answer": "Connect display devices to a test signal generator, calibrate at points along the path."
+        },
+        {
+            "id": "cts4_11",
+            "question": "What are the two main components of a projector that need to be maintained",
+            "options": [
+                "Air Filter and Lamps"
+            ],
+            "answer": "Air Filter and Lamps"
+        },
+        {
+            "id": "cts4_12",
+            "question": "Why is it very important to document all changes or update activities on an AV system whenever they are performed?",
+            "options": [
+                "To manage and maintain the system successfully over time."
+            ],
+            "answer": "To manage and maintain the system successfully over time."
+        },
+        {
+            "id": "cts4_13",
+            "question": "How does the AV manager maintain good relationships with suppliers and vendors?",
+            "options": [
+                "Document all transactions, resolve concerns in a professional manner."
+            ],
+            "answer": "Document all transactions, resolve concerns in a professional manner."
+        },
+        {
+            "id": "cts4_14",
+            "question": "What should be done when you receive a call that there is no sound in a room for an AV event?",
+            "options": [
+                "Check microphone, batteries, and power switches to assure they are on."
+            ],
+            "answer": "Check microphone, batteries, and power switches to assure they are on."
+        },
+        {
+            "id": "cts4_15",
+            "question": "Employee evaluations must include:",
+            "options": [
+                "Documentation of All standards"
+            ],
+            "answer": "Documentation of All standards"
+        },
+        {
+            "id": "cts4_16",
+            "question": "When properly addressing problems with staff performance or behavior the supervisor must be sure to:",
+            "options": [
+                "Clearly identify and document the problem behavior and instances of it."
+            ],
+            "answer": "Clearly identify and document the problem behavior and instances of it."
+        },
+        {
+            "id": "cts4_17",
+            "question": "A laptop input on a lectern is not working. The interface is connected to a 5 BNC wall plate. The interface is receiving signal and the projector is on the correct input. What is the next step in troubleshooting theissue?",
+            "options": [
+                "Reverse the H and V sync cables."
+            ],
+            "answer": "Reverse the H and V sync cables."
+        },
+        {
+            "id": "cts4_18",
+            "question": "What should be done if a touch screen control panel is reportedly locked up, frozen, or other wise reported to be not functioning?",
+            "options": [
+                "Check all connectors, confirm the screen is not in power saver mode."
+            ],
+            "answer": "Check all connectors, confirm the screen is not in power saver mode."
+        },
+        {
+            "id": "cts4_19",
+            "question": "What should you do if the audio system is emitting a high frequency hiss, or a waterfall sound?",
+            "options": [
+                "Check inputs on the mixer, make sure not set too low, check all input gains"
+            ],
+            "answer": "Check inputs on the mixer, make sure not set too low, check all input gains"
+        },
+        {
+            "id": "cts4_20",
+            "question": "Good customer service is crucial for",
+            "options": [
+                "Building good customer relationships"
+            ],
+            "answer": "Building good customer relationships"
+        },
+        {
+            "id": "cts4_21",
+            "question": "When is the best time to train users during the System Commissioning and Training Phase of a project?",
+            "options": [
+                "After the system is complete"
+            ],
+            "answer": "After the system is complete"
+        },
+        {
+            "id": "cts4_22",
+            "question": "When including GUI Layouts into the Design Documents, which of the following should also be included for optimum usability?",
+            "options": [
+                "Button-by-Button Functionality"
+            ],
+            "answer": "Button-by-Button Functionality"
+        }
+    ]
+};
